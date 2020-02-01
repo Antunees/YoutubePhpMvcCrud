ee1a8d8317e1ee670c49e30eca5110f925c306fd
Cauê Antunes <caue.apereira@gmail.com>
2020-02-01 12:49:39 -0300

    Criado o Banco de Dados
    Criado o Banco de Dados
    tabelas: Comentario e Postagem.

---
+++ banco-ded-dados.sql
@@ -0,0 +1,79 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: 01-Fev-2020 às 16:47
+-- Versão do servidor: 10.1.37-MariaDB
+-- versão do PHP: 7.3.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `serie-php-mvc-crud`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `comentario`
+--
+
+CREATE TABLE `comentario` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(200) NOT NULL,
+  `mensagem` text NOT NULL,
+  `id_postagem` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=ucs2;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `postagem`
+--
+
+CREATE TABLE `postagem` (
+  `id` int(11) NOT NULL,
+  `ttitulo` varchar(200) NOT NULL,
+  `conteudo` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=ucs2;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `comentario`
+--
+ALTER TABLE `comentario`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `postagem`
+--
+ALTER TABLE `postagem`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `comentario`
+--
+ALTER TABLE `comentario`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
